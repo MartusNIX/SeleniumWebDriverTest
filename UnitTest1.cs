a36ce396486f9ec0a6a524464b70b4c8b524b07c
Daria Martus <martusdarja@gmail.com>
2022-09-30 12:13:04 +0300

    Five task are complite

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -16,13 +16,53 @@ namespace SeleniumWebDriverTest
         }
 
         [Test]
+        public void GetPageTitle()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/");
+            Console.WriteLine(driver.Title);
+            driver.Close();
+        }
+
+        [Test]
         public void GetListLinkText()
         {                     
-            IList<IWebElement> linkList = driver.FindElements(By.CssSelector("#block_top_menu >ul > li > a"));
-            foreach (IWebElement link in linkList)
+            IList<IWebElement> linksList = driver.FindElements(By.CssSelector("#block_top_menu >ul > li > a"));
+            foreach (IWebElement link in linksList)
             {
                 Console.WriteLine(link.Text);
             }
+            driver.Close();
+        }
+
+        [Test]
+        public void GetNameAttribute()
+        {
+            IWebElement element = driver.FindElement(By.Name("search_query"));
+            Console.WriteLine(element.GetAttribute("name"));
+        }
+
+        [Test]
+        public void ClikOnTab()
+        {
+            IWebElement tabButton = driver.FindElement(By.ClassName("blockbestsellers"));
+            tabButton.Click();
+            driver.Close();
+        }
+
+        [Test]
+        public void InsertTextInSearchField()
+        {
+            IWebElement inputText = driver.FindElement(By.Id("search_query_top"));
+            inputText.SendKeys("Summer");
+            IWebElement pressSearchBtn = driver.FindElement(By.Name("submit_search"));
+            pressSearchBtn.Click();
+            driver.Close();
+        }
+        
+        [TearDown]
+        public void QuitDriver()
+        {
+            driver.Quit();
         }
     }
 }
